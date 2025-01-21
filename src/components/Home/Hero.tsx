d185fc4722d640bb51c34f155f46fa0edb43efbd
Urkel <urkelcodes7@gmail.com>
2025-01-21 13:53:12 -0800

    chnages

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -5,7 +5,7 @@ import { DollarSign, MousePointerClick } from "lucide-react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row  gap-5 items-center justify-center py-10 text-foreground relative">
+    <div className="flex flex-col-reverse md:flex-row  gap-5 items-center justify-center py-10 text-foreground relative md:overflow-hidden">
       <div className="hidden w-[800px] h-[800px] border border-primary shadow-2xl shadow-primary/80 rounded-full md:flex items-center justify-center  absolute top-0 left-1/2 translate-x-[-50%] translate-y-[5%]  animate-pulse -z-10">
         <div className="w-[600px] h-[600px] border border-primary shadow-2xl shadow-primary/80 rounded-full flex items-center justify-center">
           <div className="w-[400px] h-[400px] border border-primary shadow-2xl shadow-primary/80 rounded-full flex items-center justify-center"></div>
