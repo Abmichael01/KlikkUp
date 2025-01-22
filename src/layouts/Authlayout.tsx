41367d145af1e1944359b8c86d35ae7450dad5b3
Urkel <urkelcodes7@gmail.com>
2025-01-22 05:27:28 -0800

    changes

--- src/layouts/Authlayout.tsx
+++ src/layouts/Authlayout.tsx
@@ -6,13 +6,13 @@ import { Outlet } from "react-router";
 const Authlayout: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-[100vh] h-[100vh] overflow-hidden">
-      <div className="relative w-1/2 flex justify-center items-center">
-        <CircleWave className="top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]" />
-        <div className="scale-[4]">
+      <div className="relative w-1/2 hidden justify-center items-center md:flex">
+        <CircleWave className="top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] scale-[0.7] md:scale-100" />
+        <div className="scale-[4] sm:scale-[2.5]">
             <Logo />
         </div>
       </div>
-      <div className="w-1/2 h-full bg-white backdrop-blur-sm flex justify-center items-center px-20">
+      <div className="flex-grow h-full bg-white backdrop-blur-sm flex justify-center items-center sm:px-20 min-[500px]:px-14 px-5">
         <Outlet />
       </div>
     </div>
